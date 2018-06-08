f6eb8b9c678728b7abcfc152a589e37357a1ddf3
Meija Lohiniva <mlohiniva@hotmail.com>
2018-06-08 14:44:20 +0300

    Added comments + changed naming

--- WS2/solutions/R0333-ws2_solutions.js
+++ WS2/solutions/R0333-ws2_solutions.js
@@ -1,33 +1,43 @@
+//code for exercise1
 var exercise1 = new Vue({
     el: '#exercise1',
     data: {
         isOpen: false
     }
 })
+//code for exercise2
 var exercise2 = new Vue ({
     el: '#exercise2',
     data: {
-        isOpen: false
+        isOpen: false,
+        calculation: ''
+    },
+    computed: {
+        calculation += 
     }
 })
+//code for exercise3
 var exercise3 = new Vue({
     el: '#exercise3',
     data: {
         isOpen: false
     }
 })
+//code for exercise4
 var exercise4 = new Vue({
     el: '#exercise4',
     data: {
         isOpen: false
     }
 })
+//code for exercise5
 var exercise5 = new Vue({
     el: '#exercise5',
     data: {
         isOpen: false
     }
 })
+//code for exercise6
 var exercise6 = new Vue({
     el: '#exercise6',
     data: {
