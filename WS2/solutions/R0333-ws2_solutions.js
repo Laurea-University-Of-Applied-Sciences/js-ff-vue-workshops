f4e66f0936aa99f8c62be33d6c1656eaf401c821
Meija Lohiniva <mlohiniva@hotmail.com>
2018-09-28 14:12:54 +0300

    Added comments

--- WS2/solutions/R0333-ws2_solutions.js
+++ WS2/solutions/R0333-ws2_solutions.js
@@ -1,18 +1,18 @@
-//code for exercise 4
-Vue.component('test-component', {
-    props: ['title', 'text'],
-    data: function () {
+// code for exercise 4 - Components
+Vue.component('test-component', { //create a new component called test-component
+    props: ['title', 'text'], // define properties title and text for test-component
+    data: function () { // data should always be defined as a function so each instance of the component has their own values
         return {
             hidden: false
         }
     },
     template: '<div><h2>{{ title }}<button v-on:click="hidden=!hidden" class="left-margin">Toggle text.</button></h2><p v-show="!hidden">{{ text }}</p></div>'
 })
-//code for exercise 5
+//code for exercise 5 - Mixins
 // define a mixin object
 var log = {
-    created: function () {
-        this.log()
+    created: function () { // created hook is called when component is created 
+        this.log() // call method log()
     },
     data: function () {
         return {
@@ -21,16 +21,12 @@ var log = {
     },
     methods: {
         log: function () {
-            console.log(this.name + ' called ' + Date())
+            console.log(this.name + ' called ' + Date()) // add a note in console log
         }
     }
 }
-//code for exercise 6
-Vue.directive("red",{
-   bind(e1, binding, vnode) {
-      e1.style.backgroundColor = "red";
-   }
-});
+// code for exercise 5 - Mixins
+// create three components that use mixins
 Vue.component('mixin-component', {
     template: '<p>This is component</p>',
     data: function () {
@@ -77,37 +73,44 @@ Vue.component('exercise', {
     },
     template: '<h2 v-on:click="isOpen = !isOpen">Exercise {{ number }} - {{ title }}</h2>'
 })
-//code for exercise1
+// code for exercise 6 - Custom Directives
+// define a custom directive
+Vue.directive("red",{
+   bind(e1, binding, vnode) { // hook bind when directive is added to an element
+      e1.style.backgroundColor = "red"; // change the elements background red
+   }
+});
+// code for exercise1 - Events
 var exercise1 = new Vue({
     el: '#exercise1',
     data: {
         isOpen: false,
-        text: '',
-        keyCount: 0,
-        enter: ''
+        text: '', // this property is bound to the input 
+        keyCount: 0, // this property holds the number of characters i.e. the length of text in the input, bound to a span below the input
+        enter: '' // this property holds the text Enter clicked if enter has been clicked
     },
     methods: {
-        handleKeydown: function() {
-            this.keyCount = this.text.length;
+        handleKeydown: function() { // user presses a key down (if a user holds down a key, keydown is triggered repeatedly but keyup only once)
+            this.keyCount = this.text.length; // check the length of the text in input and update the length
         },
-        enterClicked: function() {
-            this.enter = "Enter clicked";
+        enterClicked: function() { // user clicks enter 
+            this.enter = "Enter clicked"; // update the text in a span
         }
     }
 })
-//code for exercise2
+// code for exercise2 - Computed Properties
 var exercise2 = new Vue ({
     el: '#exercise2',
     data: {
         isOpen: false,
-        runningMon: 0,
+        runningMon: 0, // variables for running are defined as separate variables for each weekday
         runningTue: 0,
         runningWed: 0,
         runningThu: 0,
         runningFri: 0,
         runningSat: 0,
         runningSun: 0,
-        gym : [
+        gym : [ // values for gym hours are defined as an array
             { value: 0 },
             { value: 0 },
             { value: 0 },
@@ -118,16 +121,16 @@ var exercise2 = new Vue ({
         ]
     },
     computed: {
-        runningTotal: function() {
+        runningTotal: function() { // computed property to calculate total hours of running
             return this.runningMon + this.runningTue + this.runningWed + this.runningThu + this.runningFri + this.runningSat + this.runningSun;
         },
-        gymTotal : function() {
+        gymTotal : function() { // computed property to calculate total hours of gym
             return this.gym.reduce((acc, item) => acc + item.value, 0);
         },
-        weekTotal : function() {
+        weekTotal : function() { // computed property to calculate total hours of the week
             return this.runningTotal + this.gymTotal;
         },
-        status : function() {
+        status : function() { // computed property for week status (poor/intermediate/good)
             if(this.weekTotal < 10) {
                 return 'poor';
             } else if(this.weekTotal < 20) {
@@ -138,19 +141,19 @@ var exercise2 = new Vue ({
         }   
     }
 })
-//code for exercise3
+// code for exercise3 - AJAX
 var exercise3 = new Vue({
     el: '#exercise3',
     data: {
         isOpen: false,
-        results: [],
-        GBP: '',
+        results: [], // this property is to store the rates got from API
+        GBP: '', 
         USD: '',
         amount: 0
     },
     methods: {
         //using fetch
-        getRates: function() {
+        getRates: function() { // get rates from API
             var app = this;
             fetch('https://api.exchangeratesapi.io/latest?symbols=USD,GBP').then(function (response) { 
                 return response.json();
@@ -169,32 +172,32 @@ var exercise3 = new Vue({
         }
     },
     computed: {
-        amountGBP: function() {
+        amountGBP: function() { // computed property for the amount in British Pounds
             if(this.GBP == '') {
                 this.getRates();
             }
             return this.amount * this.GBP;
         },
-        amountUSD: function() {
+        amountUSD: function() { // computed property for the amount in US Dollars
             return this.amount * this.USD;
         }
     }
 })
-//code for exercise4
+// code for exercise4 - Components
 var exercise4 = new Vue({
     el: '#exercise4',
     data: {
         isOpen: false
     }
 })
-//code for exercise5
+// code for exercise5 - Mixins
 var exercise5 = new Vue({
     el: '#exercise5',
     data: {
         isOpen: false
     }
 })
-//code for exercise6
+// code for exercise6 - Custom Directives
 var exercise6 = new Vue({
     el: '#exercise6',
     data: {
